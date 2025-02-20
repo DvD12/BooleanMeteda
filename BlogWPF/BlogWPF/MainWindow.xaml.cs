3bd1ddae121e413b05d2fd4a459201c865ee4589
DvD <dvd1202@gmail.com>
2025-02-20 18:03:49 +0100

    - WPF

---
+++ BlogWPF/BlogWPF/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BlogWPF;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+	private void OnLoginBtnClick(object sender, RoutedEventArgs e)
+	{
+		this.MainFrame.NavigationService.Navigate(new Uri("LoginPage.xaml", UriKind.Relative));
+	}
+
+	private void OnUserBtnClick(object sender, RoutedEventArgs e)
+	{
+		this.MainFrame.NavigationService.Navigate(new Uri("UserPage.xaml", UriKind.Relative));
+	}
+}
